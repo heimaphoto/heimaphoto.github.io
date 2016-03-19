6213e99258fdc187f2e3acc9f8c0866ca5660499
wangbin <wbwuxi@gmail.com>
2016-03-19 15:57:07 +0800

    add index.html tongji.js

---
+++ tongji.js
@@ -0,0 +1,7 @@
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "//hm.baidu.com/hm.js?596a2ebb137d1de7bc70a6910742474c";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
